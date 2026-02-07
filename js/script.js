177d8479029934cc0fd780d7a22618d136feb29f
용선경 <133983127+Yong-seon-gyeong@users.noreply.github.com>
2026-02-07 23:56:46 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// tab
+function openTab(tabName) {
+  document.querySelectorAll('.tab, .tabs').forEach(function (el) {
+    el.classList.remove('on');
+  });
+  document.getElementById(tabName).classList.add('on');
+  document.querySelector('.tabs[onclick="openTab(\'' + tabName + '\')"]').classList.add('on');
+}
+
+//  counter
+if ($('.single-counter').length) { // 1번: .single-counter가 페이지에 있는지 확인
+    $('.counter').counterUp({      // 2번: .counter 클래스를 가진 요소들에 기능 부여
+      delay: 10,
+      time: 1000
+    });
+}
